914ca42103d6e946155f45c87a19cf7723f984b9
Mishanyyy228 <mananev13@gmail.com>
2025-01-29 00:25:57 +0300

    реализован функционал окна registration

--- Registration.xaml.cs
+++ Registration.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using lab.Validation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using lab.Validation;
 
 namespace lab
 {
@@ -22,6 +24,144 @@ namespace lab
         public Registration()
         {
             InitializeComponent();
+        }
+        private void Name_user_GotFocus_1(object sender, RoutedEventArgs e)
+        {
+            if (Name_user.Text == "Введите имя пользователя")
+            {
+                Name_user.Text = string.Empty;
+                Name_user.Foreground = Brushes.Black;
+            }
+        }
+
+        private void Email_user_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (Email_user.Text == "Введите почту")
+            {
+                Email_user.Text = string.Empty;
+                Email_user.Foreground = Brushes.Black;
+            }
+        }
+
+        private void Pass_user_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (Pass_user.Text == "Введите пароль")
+            {
+                Pass_user.Text = string.Empty;
+                Pass_user.Foreground = Brushes.Black;
+            }
+        }
+
+        private void Pass_user1_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (Pass_user1.Text == "Повторите пароль")
+            {
+                Pass_user1.Text = string.Empty;
+                Pass_user1.Foreground = Brushes.Black;
+            }
+        }
+
+        private void Name_user_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (Name_user.Text == "")
+            {
+                Name_user.Text = "Введите имя пользователя";
+                Name_user.Foreground = Brushes.Gray;
+            }
+        }
+
+        private void Email_user_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (Email_user.Text == "")
+            {
+                Email_user.Text = "Введите почту";
+                Email_user.Foreground = Brushes.Gray;
+            }
+        }
+
+        private void Pass_user_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (Pass_user.Text == "")
+            {
+                Pass_user.Text = "Введите пароль";
+                Pass_user.Foreground = Brushes.Gray;
+            }
+        }
+
+        private void Pass_user1_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (Pass_user1.Text == "")
+            {
+                Pass_user1.Text = "Повторите пароль";
+                Pass_user1.Foreground = Brushes.Gray;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window w2 = new LogIn();
+            Hide();
+            w2.Show();
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (Name_user.Text != "Введите имя пользователя")
+            {
+                if (Email_user.Text != "Введите почту")
+                {
+                    if (Pass_user.Text != "Введите пароль")
+                    {
+                        string email = Email_user.Text;
+                        string password = Pass_user.Text;
+                        string name = Name_user.Text;
+                        string password1 = Pass_user1.Text;
+
+                        bool isEmailValid = email.ValidateEmail();
+                        bool isPasswordValid = password.ValidatePassword();
+                        bool isNameValid = name.ValidateName();
+                        bool isPasswordValid1 = password1.ValidatePassword();
+
+                        if (!isEmailValid)
+                        {
+                            MessageBox.Show("Некорректный формат почты.");
+                        }
+                        if (!isPasswordValid)
+                        {
+                            MessageBox.Show("Пароль должен быть не менее 6 символов.");
+                        }
+                        if (!isPasswordValid1)
+                        {
+                            MessageBox.Show("Пароль должен быть не менее 6 символов.");
+                        }
+                        if (!isNameValid)
+                        {
+                            MessageBox.Show("Имя должно содержать не менее 3 символов.");
+                        }
+                        if (password != password1)
+                        {
+                            MessageBox.Show("Пароли должны совпадать");
+                        }
+                        if (isEmailValid & isNameValid & isPasswordValid & password == password1)
+                        {
+                            Window w2 = new MainEmpty();
+                            Hide();
+                            w2.Show();
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Поле пароль пустое");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Поле почта пустое");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Поле имя пустое");
+            }
         }
     }
 }
