42ae21db3684cedffc0c7a3d748cd6d2d6622d01
Mishanyyy228 <mananev13@gmail.com>
2025-01-29 00:27:44 +0300

    реализован функционал окна login

--- LogIn.xaml.cs
+++ LogIn.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using lab.Validation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,85 @@ namespace lab
         public LogIn()
         {
             InitializeComponent();
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Window w2 = new Registration();
+            Hide();
+            w2.Show();
+        }
+
+        private void Password_user11_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (Password_user11.Text == "Введите пароль")
+            {
+                Password_user11.Text = string.Empty;
+                Password_user11.Foreground = Brushes.Black;
+            }
+        }
+
+        private void Password_user11_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (Password_user11.Text == "")
+            {
+                Password_user11.Text = "Введите пароль";
+                Password_user11.Foreground = Brushes.Gray;
+            }
+        }
+
+        private void Pochta_user11_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (Pochta_user11.Text == "student12@gmail.com")
+            {
+                Pochta_user11.Text = string.Empty;
+                Pochta_user11.Foreground = Brushes.Black;
+            }
+        }
+
+        private void Pochta_user11_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (Pochta_user11.Text == "")
+            {
+                Pochta_user11.Text = "student12@gmail.com";
+                Pochta_user11.Foreground = Brushes.Black;
+            }
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Pochta_user11.Text != "student12@gmail.com")
+            {
+                if (Password_user11.Text != "Введите пароль")
+                {
+                    string email = Pochta_user11.Text;
+                    string password = Password_user11.Text;
+
+                    bool isEmailValid = email.ValidateEmail();
+                    bool isPasswordValid = password.ValidatePassword();
+
+                    if (!isEmailValid)
+                    {
+                        MessageBox.Show("Некорректный формат почты.");
+                    }
+                    if (!isPasswordValid)
+                    {
+                        MessageBox.Show("Пароль должен быть не менее 6 символов.");
+                    }
+                    if (isEmailValid | isPasswordValid)
+                    {
+                        Window w2 = new MainEmpty();
+                        Hide();
+                        w2.Show();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Поле пароль пустое!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Поле почта пустое!");
+            }
         }
     }
 }
